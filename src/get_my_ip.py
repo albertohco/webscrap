1b83bb8e369561a79605c8ffe9232a5ab459b061
albertohco <acordeiro@3anet.com.br>
2024-11-03 19:01:11 -0300

    Alteração de proxy local

--- src/get_my_ip.py
+++ src/get_my_ip.py
@@ -1,14 +1,14 @@
 import requests
 
-# proxy = {
-#    'http': 'http://localhost:8080',
-#    'https': 'http://localhost:8080',
-# }
-
 proxy = {
-    'http': 'http://131.196.42.95:667',
-    'https': 'http://131.196.42.95:667',
+    'http': 'http://localhost:8080',
+    'https': 'http://localhost:8080',
 }
+
+# proxy = {
+#    'http': 'http://131.196.42.95:667',
+#    'https': 'http://131.196.42.95:667',
+# }
 
 url = "http://lumtest.com/myip.json"
 url2 = "https://www.globo.com"
